21b8eb42873d5c01417e8059ceb3583d6d7edc4b
Tawmu <Tawmu@users.noreply.github.com>
2023-02-28 13:24:58 +0000

    Update entrypoint.sh

--- pdns-recursor/overlay/entrypoint.sh
+++ pdns-recursor/overlay/entrypoint.sh
@@ -21,4 +21,5 @@ echo "Starting PowerDNS Recursor..."
     --api-key=$PDNS_API_KEY \
     --network-timeout=5000 \
     --allow-from="${ALLOW_FROM}" \
-    --forward-zones-recurse=".=${MASTER_ADDR}:5300;${UPSTREAM_RECURSOR}"
+    --forward-zones=".={$MASTER_ADDR}:5300"
+    --forward-zones-recurse=".=${UPSTREAM_RECURSOR}"
